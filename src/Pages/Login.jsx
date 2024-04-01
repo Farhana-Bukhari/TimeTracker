bc0d8f064220ce4e882577e841516b77b58da2cf
Farhana Bukhari <farhanabukhari20@gmail.com>
2024-04-01 16:02:24 +0530

    responsiveness

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -9,12 +9,12 @@ function Login() {
     <div className="login-page">
       <div className="login-section">
         <div
-          className="login d-flex justify-content-center align-items-center vh-100  
+          className="login d-flex justify-content-center align-items-center   
     "
         >
-          <div className="form_container p-5 rounded ">
+          <div className="form_container  rounded ">
             <h1 className="form-heading">Welcome Back!</h1>
-            <form action="">
+            <form action="" className="">
               <h3 className=" Signin text-left">Login to your account</h3>
               <div className="mb-4">
                 <label htmlFor="email" className="label-text mb-2">
@@ -36,34 +36,35 @@ function Login() {
                   className="form-control"
                 />
               </div>
-              <div className="mb-2">
-                <input
-                  type="checkbox"
-                  id="check"
-                  className="custom-control custom-checkbox"
-                />
-                <label htmlFor="check" className="custom-input-label ms-2">
-                  Remember me
-                </label>
+              <div className="rememberMe-forgotPassword ">
+                <div className="rememberMe-holder  d-flex align-items-center justify-content-start w-100">
+                  <input
+                    type="checkbox"
+                    id="check"
+                    className="custom-control custom-checkbox"
+                  />
+                  <label htmlFor="check" className="custom-input-label ms-2">
+                    Remember me
+                  </label>
+                </div>
+                <div className="forgot-password-holder d-flex align-items-center justify-content-start w-100">
+                  <p className=" mb-2 ">
+                    <a href="" className="forgot-password ">
+                      I forgot my password
+                    </a>
+                  </p>
+                </div>
               </div>
               <div className="d-grid">
-                <button className="btn ">Sign In</button>
+                <button className="btn">Sign In</button>
               </div>
-              <p className=" text-start mt-4">
-                <a href="" className="forgot-password ">
-                  I forgot my password
-                </a>
-                {/* <Link to="/signup" className="ms-2">
-                  Sign up
-                </Link> */}
-              </p>
             </form>
           </div>
         </div>
       </div>
       <div className="image-section  d-flex justify-content-center align-items-center">
         <div className="image-holder d-flex justify-content-center align-items-center">
-          <img src={LoginImage} alt="" className="" />
+          <img src={LoginImage} alt="" className="image-fluid" />
         </div>
       </div>
     </div>
